3f0f228a738202489b9dcd915a944a561f3604fa
YoungDriverOfTech <xiaoleliang045@gmail.com>
2020-06-11 16:22:43 +0900

    deploy

---
+++ precache-manifest.e7dc48e93320da8ed33aa04e70a72c92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994df3a0e5fa1f219deb9b82f92a1acd",
+    "url": "/book-keeping-react/index.html"
+  },
+  {
+    "revision": "da426a054bb5ad97aeb1",
+    "url": "/book-keeping-react/static/css/main.6bf163ce.chunk.css"
+  },
+  {
+    "revision": "351aeb7a9099b35ad1bb",
+    "url": "/book-keeping-react/static/js/2.b8beb48f.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/book-keeping-react/static/js/2.b8beb48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da426a054bb5ad97aeb1",
+    "url": "/book-keeping-react/static/js/main.f0f18c10.chunk.js"
+  },
+  {
+    "revision": "a65b1ab87b09acc2923e",
+    "url": "/book-keeping-react/static/js/runtime-main.d45f1a12.js"
+  }
+]);
