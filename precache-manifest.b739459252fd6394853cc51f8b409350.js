b6deee653be4024e90f437f69d1616a184b0b1e3
liang <xiaoleliang045@gmail.com>
2022-01-03 10:37:56 +0900

    deploy

---
+++ precache-manifest.b739459252fd6394853cc51f8b409350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51b38e784ec5501cc711e25ac810298",
+    "url": "/book-keeping-react/index.html"
+  },
+  {
+    "revision": "ed891e40335a65a04722",
+    "url": "/book-keeping-react/static/css/main.6bf163ce.chunk.css"
+  },
+  {
+    "revision": "351aeb7a9099b35ad1bb",
+    "url": "/book-keeping-react/static/js/2.b8beb48f.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/book-keeping-react/static/js/2.b8beb48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed891e40335a65a04722",
+    "url": "/book-keeping-react/static/js/main.a162cedd.chunk.js"
+  },
+  {
+    "revision": "a65b1ab87b09acc2923e",
+    "url": "/book-keeping-react/static/js/runtime-main.d45f1a12.js"
+  }
+]);
